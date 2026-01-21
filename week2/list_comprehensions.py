e8381ab3aa6c7c4eb113712751dd5fa93bc48791
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-01-22 00:39:13 +0500

    list comprehension q1 dn | write code in square bracs

---
+++ week2/list_comprehensions.py
@@ -0,0 +1,10 @@
+# Q5. Even Numbers Filter
+
+# Tasks:
+# Create a new list of only even numbers using:
+# List comprehension
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8]
+even_num = [num for num in nums if num % 2 == 0]
+
+print(even_num)
