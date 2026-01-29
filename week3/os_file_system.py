39efea4e43cf358c06fc42f47bab9f9cc4137aa8
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-01-29 18:15:52 +0500

    os prac, does exist, create, del file with comments

---
+++ week3/os_file_system.py
@@ -0,0 +1,26 @@
+import os
+
+#checks does path or file exist | return True
+path = "os_file_system.py"
+if os.path.exists(path):
+    print("File exist")
+else:
+    print("FIle doesn't exist.")
+
+#creating directory if not exist | return None | error if already exists.
+create_directory = "example_directory/sub_directory"
+if os.path.exists(create_directory):
+    print("File already exist.")
+else:
+    os.makedirs(create_directory)
+    print("File created successfully.")
+
+
+#deletes directory if empty | return None | error if not empty or not found
+delete_directory = "example_2"
+if os.path.exists(delete_directory):
+    # will not delete if dir contains sub_dir or files. delete only when dir empty
+    deleted = os.rmdir(delete_directory)
+    print(deleted)
+else:
+    print("No such directory found")
