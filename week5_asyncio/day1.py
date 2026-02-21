8fd6866ecd979a6db3a35ee7396c7d387af33d1e
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-02-21 14:00:32 +0500

    asyncio.create_task() practice code

--- week5_asyncio/day1.py
+++ week5_asyncio/day1.py
@@ -12,23 +12,43 @@ futures == placeholder for future vals of currently executing task
 import asyncio
 
 
-async def say_hello_async():
-    await asyncio.sleep(2)  # Simulates waiting for 2 seconds
-    print("Hello, Async World!")
+# async def say_hello_async():
+#     await asyncio.sleep(2)  # Simulates waiting for 2 seconds
+#     print("Hello, Async World!")
 
 
-async def do_something_else():
-    print("Starting another task...")
-    await asyncio.sleep(1)  # Simulates doing something else for 1 second
-    print("Finished another task!")
+# async def do_something_else():
+#     print("Starting another task...")
+#     await asyncio.sleep(1)  # Simulates doing something else for 1 second
+#     print("Finished another task!")
 
 
-async def main():
-    # Schedule both tasks to run concurrently
-    await asyncio.gather(
-        say_hello_async(),
-        do_something_else(),
-    )
+# async def main():
+#     # Schedule both tasks to run concurrently
+#     await asyncio.gather(
+#         say_hello_async(),
+#         do_something_else(),
+#     )
 
 
-asyncio.run(main())
+# asyncio.run(main())
+
+
+async def func1 ():
+    asyncio.create_task(func2())
+    print("First")
+    
+    await asyncio.sleep(2)
+    print("Second")
+
+    await asyncio.sleep(1)
+    print("Third")
+
+async def func2 ():
+    print("Fourth")
+
+    await asyncio.sleep(2)
+    print("Fifth")
+
+asyncio.run(func1())
+"""outpu ==> 1,4,2, 5, 3"""
