5a2879049c34ef5f888ac3cc180d5b24241cf49c
Aayan Ahmed <aayanahmedtejani@gmail.com>
2026-02-21 12:32:59 +0500

    oop practice person class question

--- week4/01_oop.py
+++ week4/01_oop.py
@@ -13,6 +13,23 @@ class Circle:
         print(math.pi * self.radius * self.radius)
 
 
-circle1 = Circle(3)
-circle1.calc_area()
-circle1.calc_perimeter()
+# circle1 = Circle(3)
+# circle1.calc_area()
+# circle1.calc_perimeter()
+
+# Write a Python program to create a person class. Include attributes like name, country and date of birth. Implement a method to determine the person's age.
+# import
+from datetime import date
+class Person:
+    def __init__(self, name, country, dob):
+        self.name = name
+        self.country = country
+        self.dob = dob
+    
+    def find_age(self):
+        curr_year = date.today().year
+        return int(curr_year) - int(self.dob)
+        
+p1 = Person("aayan Ahmed", "Pakistan", "2006")
+p1_age = p1.find_age()
+print(p1_age)
