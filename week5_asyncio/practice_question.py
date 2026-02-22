9e475eeef41c055c7f8b43f80b8e933d2c928b4a
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-22 11:56:18 +0000

    prac ques 3 solved

--- week5_asyncio/practice_question.py
+++ week5_asyncio/practice_question.py
@@ -1,6 +1,7 @@
 """
 01) Write a Python program that creates an asynchronous function to print "Python Exercises!" with a two second delay.
 02) Write a Python program that creates three asynchronous functions and displays their respective names with different delays (1 second, 2 seconds, and 3 seconds).
+03) Write a Python program that creates an asyncio event loop and runs a coroutine that prints numbers from 1 to 7 with a delay of 1 second each.
 """
 import asyncio
 
@@ -17,16 +18,24 @@ import asyncio
 # asyncio.run(main())
 
 """02"""
-async def display_name_with_delay(name, delay):
-    await asyncio.sleep(delay)
-    print(name)
+# async def display_name_with_delay(name, delay):
+#     await asyncio.sleep(delay)
+#     print(name)
 
-async def main():
-    tasks = [
-        display_name_with_delay("Async func 1", 1),
-        display_name_with_delay("Async func 2", 2),
-        display_name_with_delay("Async func 3", 3),
-    ]
-    await asyncio.gather(*tasks)
+# async def main():
+#     tasks = [
+#         display_name_with_delay("Async func 1", 1),
+#         display_name_with_delay("Async func 2", 2),
+#         display_name_with_delay("Async func 3", 3),
+#     ]
+#     await asyncio.gather(*tasks)
 
-asyncio.run(main())
+# asyncio.run(main())
+
+"""03"""
+async def display_numbers():
+    for r in range(1,8):
+        await asyncio.sleep(1) 
+        print(r)
+
+asyncio.run(display_numbers())
