3ece56e4f1ba6a24ceb6859996bbb94b3cfc4980
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-24 23:06:54 +0000

    basic post request done

--- week6_httpx/client.py
+++ week6_httpx/client.py
@@ -1,11 +1,26 @@
 import httpx
 
+
+# basic get request
 def get_data():
     response = httpx.get("https://httpbin.org/get")
     print(f"Status Code: {response.status_code}")
     print(f"Headers: {response.headers}")
     print(f"Content: {response.text}")
     return response
-    
+
+#post request
+def create_task():
+    url = "https://httpbin.org/post"
+    payload = {"title": "Learning httpx", "completed": False}
+
+    response = httpx.post(url, json=payload)
+
+    print(f"Status Code: {response.status_code}")
+    print(f"Response: {response.json()}")
+
+    return response
+
 if __name__ == "__main__":
-    print(get_data())
+    # print(get_data())
+    print(create_task())
