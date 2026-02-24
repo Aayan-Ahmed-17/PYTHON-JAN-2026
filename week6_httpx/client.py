43ab188cdbdbc18d837b100c5c7c4094e0457508
Aayan-Ahmed-17 <152885143+Aayan-Ahmed-17@users.noreply.github.com>
2026-02-24 22:49:53 +0000

    get request without async | follow guide from scratch

--- week6_httpx/client.py
+++ week6_httpx/client.py
@@ -1,13 +1,9 @@
-import asyncio
 import httpx
 
-async def call_api():
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get("https://jsonplaceholder.typicode.com/posts/1")
-            print(response.status_code)
-            print(response.json())
-    except Exception as e:
-        print("Error:", e)
+def get_data():
+    response = httpx.get("https://httpbin.org/get")
+    return response.json()
 
-asyncio.run(call_api())
+if __name__ == "__main__":
+    data = get_data()
+    print(data)
